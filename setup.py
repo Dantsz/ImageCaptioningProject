0aa1969ffbb8bb48e6a203b47323d3e97cde35ac
Dan Costinas <danscostinas@gmail.com>
2025-04-20 22:02:05 +0300

    Fix RandomResizeCrop

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="EncoderDecoderProjectComponents",
-    version="0.3.22",
+    version="0.3.24",
     packages=find_packages(),
     install_requires=[
         "loguru"
