88237627bd596551e2fce4f242056f96ae1108e3
Dan Costinas <danscostinas@gmail.com>
2025-04-25 19:30:42 +0300

    Reduce dropout and add more cross attention

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="EncoderDecoderProjectComponents",
-    version="0.3.54",
+    version="0.3.55",
     packages=find_packages(),
     install_requires=[
         "loguru"
