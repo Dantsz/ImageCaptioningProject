01928bfa500822f552a660ff27dd033efa131524
Dan Costinas <danscostinas@gmail.com>
2025-04-26 09:56:36 +0300

    Reduce transformer blocks to 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="EncoderDecoderProjectComponents",
-    version="0.3.55",
+    version="0.3.56",
     packages=find_packages(),
     install_requires=[
         "loguru"
